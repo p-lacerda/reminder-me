0ff70021d29590cd1c3283e12102d032b52682cd
tassolacerda <xcloudgameemail@gmail.com>
2021-11-25 19:51:20 -0300

    add dark button

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import {IconButton, Input,  Center } from "@chakra-ui/react"
+import {IconButton, Input,  Center, HStack } from "@chakra-ui/react"
 import { AddIcon } from '@chakra-ui/icons'
 import { Button, ButtonGroup } from "@chakra-ui/react"
 import { Heading } from "@chakra-ui/react"
@@ -10,39 +10,37 @@ function AddTodo() {
 
   return (
     <Center>
-    <form>
-      <label htmlFor="newTodo">
-        <Heading
-          color=""
-          as="h3"
-          size="lg"
-          marginTop="5px"
-          textAlign="center"
+      <form>
+          <Heading
+            color=""
+            as="h3"
+            marginTop="1.5rem"
+            textAlign="center"
+          >
+            Todo App
+          </Heading>
+          <HStack mt='5'>
+            <Input
+              placeholder="Add your new todo..."
+              marginRight="5px"
+              maxW="230px"
+              size="md"
+              value={todoText}
+              onChange={(e) => (setTodoText(e.target.value))}
+            />
+        <IconButton
+          icon={<AddIcon />}
+          colorScheme="green"
+          variant={ todoText.length > 3 ? "solid" : "outline" }
+          disabled={ todoText.length > 3 ? false : true }
+          onClick={() => createList()}
         >
-          Todo App
-        </Heading>
-        <Input
-          placeholder="Add your new todo..."
-          marginTop="5px"
-          marginRight="5px"
-          maxW="230px"
-          size="md"
-          value={todoText}
-          onChange={(e) => (setTodoText(e.target.value))}
-        />
-      </label>
-      <IconButton
-        icon={<AddIcon />}
-        colorScheme="green"
-        variant={ todoText.length > 3 ? "solid" : "outline" }
-        disabled={ todoText.length > 3 ? false : true }
-        onClick={() => createList()}
-      >
-        Adicionar tarefa
-      </IconButton>
+          Adicionar tarefa
+        </IconButton>
+      </HStack>
     </form>
-    </Center>
-  );
+</Center>
+);
 }
 
 export default AddTodo;
